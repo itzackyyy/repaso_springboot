a00775c8c36f43ca37fb2043713808a446e0436b
itzackyyy <brun.soto@duocuc.cl>
2026-04-15 11:06:23 -0400

    se remueven lineas inutiles

--- src/main/java/com/zacky/pcfactory/Cliente/Controller/ClienteController.java
+++ src/main/java/com/zacky/pcfactory/Cliente/Controller/ClienteController.java
@@ -1,15 +1,19 @@
 package com.zacky.pcfactory.Cliente.Controller;
 
 
-import com.zacky.pcfactory.Cliente.Model.ClienteModel;
-import com.zacky.pcfactory.Cliente.Service.ClienteService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.zacky.pcfactory.Cliente.Model.ClienteModel;
+import com.zacky.pcfactory.Cliente.Service.ClienteService;
 
 @RestController()
 @RequestMapping("/api/v1")
